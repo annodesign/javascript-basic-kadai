010a1a5e58160b964f8f37f6c9d5b1b340a55be1
annodesign <anno.design785@gmail.com>
2024-09-11 17:29:29 +0900

    課題7提出用にコメントアウトを追加

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,8 +1,8 @@
 // 変数numに0～4までのランダムな整数を代入する
 // let num = Math.floor(Math.random() * 30);
 // 出力の確認↓
-let num = 15;
-console.log(num);
+// let num = 15;
+// console.log(num);
 
 // 変数(num)が3と5の倍数の場合： “3と5の倍数です”
 if (num % 15 === 0) {
@@ -37,16 +37,16 @@ else {
 // ------------------------------------------------
 // 変数numに0～4までのランダムな整数を代入する
 
-num = Math.floor(Math.random() * 5);
+// num = Math.floor(Math.random() * 5);
 // 変数numの最初の値を出力する（確認用）
 
-console.log('最初の値は' + num + 'です');
+// console.log('最初の値は' + num + 'です');
 // 変数numの値が0以外である間、変数numの値を出力し続ける
-while (num !== 0) {
+// while (num !== 0) {
 
   // 変数numに0～4までのランダムな整数を再代入する
-  num = Math.floor(Math.random() * 5);
+  // num = Math.floor(Math.random() * 5);
 
     // 次の条件式で比較される、変数numの現在の値を出力する
-  console.log('現在の値は' + num + 'です。'); 
-}
+  // console.log('現在の値は' + num + 'です。'); 
+// }
