39caf1c91fe00704be6c7d9470477b9a73f372d7
annodesign <anno.design785@gmail.com>
2024-09-12 09:40:16 +0900

    課題が見やすいよう修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,4 @@
-// const now = new Date(); // 現在の日付と時間を取得
+// 基本情報-----------------------------------------------------------------------------
 
 // const year = now.getFullYear();       // 年
 // const month = now.getMonth() + 1;     // 月 (0-11なので+1する)
@@ -9,50 +9,41 @@
 
 // const formattedDateTime = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
 // console.log(formattedDateTime); // 例: 2024/9/7 15:30:45
+// －－－－－－－－－－－－－－－-----------------------------------------------------------------------------
 
 
-// --------------------------------------------------自己作成------------------------------------------
-// --------------------------------------------------アロー関数-----------------------------------------
+// 今日の日付を取得
+let today = new Date();
 
-const now = new Date(); // 現在の日付と時間を取得
+// 年、月、日を取得
+let year = today.getFullYear();
+let month = today.getMonth() + 1; // 月は0から始まるため、1を足す
+let day = today.getDate();
 
-const getYear = () => now.getFullYear();        // 年を取得
-const getMonth = () => now.getMonth() + 1;      // 月を取得 (0-11なので+1)
-const getDay = () => now.getDate();             // 日を取得
-
-// オブジェクト-------------------------------------------------------------------------------
-// 下記の入力はまちがいです。
-// console today = { getYear(), getMonth(), getDay(), };
-// console.log(today);
-//-------------------------------------------------------------------------------
-
-const year = getYear();
-const month = getMonth();
-const day = getDay();
-
-const formattedDateTime = `${year}/${month}/${day}`;
-console.log(formattedDateTime);
+// 日付を「2023年8月22日」の形式で表示
+console.log(`${year}年${month}月${day}日`);
 
 
 
 
+// アロー関数で記載してみる－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
+const sayGoodMorning = () => {
+  const date = new Date();
 
-// －－－－－－－－－－－－－－－－－－－－－－－－－－正解データアロー関数
-// const sayGoodMorning = () => {
-//   const date = new Date();
-//   const month = date.getMonth() + 1; // 月は0から11で表されるため、+1する
-//   const day = date.getDate();
-//   const year = date.getFullYear();
-//   const hour = date.getHours();
-//   const minutes = date.getMinutes();
-//   const seconds = date.getSeconds();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 月は0から11で表されるため、+1する
+  const day = date.getDate();
+  
+  // const hour = date.getHours();
+  // const minutes = date.getMinutes();
+  // const seconds = date.getSeconds();
 
-//   console.log(`${year}年${month}月${day}日 ${hour}時${minutes}分${seconds}秒`);
-//   console.log('おはようございます！');
-//   console.log('昨日はよく眠れましたか？');
-//   console.log('今日も一日頑張りましょう！');
-// };
+  console.log(`${year}年${month}月${day}日`);
+  // console.log('おはようございます！');
+  // console.log('昨日はよく眠れましたか？');
+  // console.log('今日も一日頑張りましょう！');
+};
 
-// sayGoodMorning();
+sayGoodMorning();
 // －－－－－－－－－－－－－－－－－－－－－－－－－－上記の修正では、アロー関数内で日時を取得し、
-// それを元に特定のメッセージを表示しています。最後に、sayGoodMorning()を呼び出すことで、結果が表示されます。
+// それを元に特定のメッセージを表示しています。最後に、sayGoodMorning()を呼び出すことで、結果が表示。
