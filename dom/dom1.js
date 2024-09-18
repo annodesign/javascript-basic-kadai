91fed3112f500835a987f8cf46aa5050d6baafe3
annodesign <anno.design785@gmail.com>
2024-09-18 14:10:23 +0900

    スクロールTOP修正

--- dom/dom1.js
+++ dom/dom1.js
@@ -1,11 +1,13 @@
 // 20章　スクロールしてボタン表示
 // windowオブジェクトを指定します。windowオブジェクト15章復習
-window.addEventListener(scroll, () => {
-    // back-btnというidを持つHTML要素を取得し、定数に代入する
-  const backBtn = document.getElementById('back-btn');
 
+
+// back-btnというidを持つHTML要素を取得し、定数に代入する
+const backBtn = document.getElementById('back-btn');
+ 
+// 画面がスクロールされたときにイベント処理を実行する
+window.addEventListener('scroll', () => {
   // 画面のスクロール量をpx（ピクセル）数で取得する
-  // （document.scrollingElement.scrollTopプロパティを使用します。）
   const scrollValue = document.scrollingElement.scrollTop;
 
   // 画面のスクロール量が300px以上であれば、「TOPに戻る」ボタンを表示する
@@ -16,4 +18,4 @@ window.addEventListener(scroll, () => {
   else {
     backBtn.style.display = 'none';
   }
-})
+});
