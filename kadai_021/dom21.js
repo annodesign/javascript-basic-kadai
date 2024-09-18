afb882da013d9170c8a9bf659258cae15d544ebb
annodesign <anno.design785@gmail.com>
2024-09-18 14:05:06 +0900

    add

--- kadai_021/dom21.js
+++ kadai_021/dom21.js
@@ -18,3 +18,39 @@ setTimeout(() => {
 // 同期処理を実行する
 console.log('処理5');
 console.log('処理6');
+
+
+// 22章エラーハンドリング
+// --------------------------------------------------------------
+// 　　　　発生する可能性のある例外（エラー）を捕捉し、適切な処理ができる
+try {
+   // 例外が発生する可能性のあるコード　notFunction
+  notFunction()
+} catch(error) {
+  // 例外が発生した場合の処理
+  console.log('--- output error ---')
+  console.log(error.name)
+  console.log(error.message)
+}
+
+
+// 例外の発生の有無にかかわらず必ず実行させたい処理がある場合
+try {
+   // 例外が発生する可能性のある処理
+  console.log('hello!')
+} catch(error) {
+   // 例外が発生した場合の処理
+  console.log(error.name)
+  console.log(error.message)
+} finally {
+   //必ず実行される処理
+  console.log('end!')
+}
+
+// 数値の入力忘れなどのミスを確認する時
+
+let name = "Alice";
+let age = "";
+
+console.log("Name:", name); // 変数の値を表示
+console.log("Age:", age);
