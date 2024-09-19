10e736259525ceb452ee8fa31c655043390cb218
annodesign <anno.design785@gmail.com>
2024-09-19 22:52:18 +0900

    課題提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,5 +2,6 @@ const text = document.getElementById('text');
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
+  // 「element.innerHTML = ‘文字列’」で中身を変える。
   text.innerHTML = "ボタンをクリックしました";
 })
